53a639126ae4ada639d1575b7858d9594410f0d9
ikkorokoro <ikkorokoro@icloud.com>
2021-07-16 23:07:32 +0900

    practice6--2 問5

--- practice5.rb
+++ practice5.rb
@@ -4,9 +4,12 @@ puts menu[:coffee_latte]
 menu = {"モカ": "チョコレートシロップ" }
 p menu[:"モカ"] 
 
-menu[:tea] = 300
-puts menu
+# menu[:tea] = 300
+# puts menu
 
 #6-2
 menu.delete(:coffee_latte)
 p menu
+if menu[:tea].nil?
+  puts '紅茶はありませんか？'
+end
